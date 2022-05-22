470387956ecccfa6261f110b26667d79aa331448
Adam Ameen <adamameen5@gmail.com>
2022-05-23 01:25:07 +0530

    Fixed an issue where data did not get inserted

--- insertReview.php
+++ insertReview.php
@@ -2,18 +2,18 @@
 
     $con=mysqli_connect("127.0.0.1","root","root","WhatAReview",8889);
 
-    $originalReview = $_POST['originalReview'];
-    $summarisedReview = $_POST['summarisedReview'];
-    $sarcasmDetected = $_POST['sarcasmDetected'];
-    $ourRating = $_POST['ourRating'];
+    $originalReview = mysqli_real_escape_string($con,$_POST['originalReview']);
+    $summarisedReview = mysqli_real_escape_string($con,$_POST['summarisedReview']);
+    $sarcasmDetected = mysqli_real_escape_string($con,$_POST['sarcasmDetected']);
+    $ourRating = mysqli_real_escape_string($con,$_POST['ourRating']);
 
     $insert="INSERT INTO `reviews`(`review_id`, `original_review`, `summarised_review`, `sarcasm_detected`, `rating_generated`) VALUES (null,'$originalReview','$summarisedReview','$sarcasmDetected','$ourRating')";
-    echo $originalReview.$summarisedReview.$sarcasmDetected.$ourRating;
+    
     try {
-        mysqli_query($con,$insert);
+        mysqli_query($con,$insert) or die(mysqli_error($con));
         header("Location: reviews.php?");
     } catch (Exception $e){
-        echo "ERROR: Could not able to execute $insert. " . mysqli_error($con);
+        echo "ERROR: Unable to execute $insert. " . mysqli_error($con);
     }
     
 ?>
